bfb2b4ec70660fe90119e9aaab5f843ffb640dd6
sirbdhy46-alt <sirbdhy46@gmail.com>
2026-04-14 02:49:54 +0530

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,22 @@
+import logging
+import sys
+from datetime import datetime
+
+def setup_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter(
+            "[%(asctime)s] [%(levelname)s] %(name)s: %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
+
+bot_logger = setup_logger("DiscordBot")
